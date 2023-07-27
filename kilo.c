edc70196f55c7b3c6648f6263272beb1d8e6c061
Priyam Srivastava <ipriyam26@debian.ipriyam26.com>
2023-07-27 14:20:04 +0530

    chore(kilo.c): add clear screen functionality when pressing CTRL + q to improve user experience

--- kilo.c
+++ kilo.c
@@ -51,6 +51,8 @@ void editorProcessKeypress() {
     char c = editorReadKey();
     switch (c) {
     case CTRL_KEY('q'):
+        write(STDOUT_FILENO, "\x1b[2J", 4);
+        write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
         break;
     }
