244cae2f6ffd5cba5864451167512075b997ea9e
Priyam Srivastava <ipriyam26@debian.ipriyam26.com>
2023-07-27 15:07:50 +0530

    fix(kilo.c): fix typo in getCursorPosition function, missing semicolon at the end of the return statement refactor(kilo.c): remove unnecessary code in getWindowSize function, replace editorReadKey() with getCursorPosition(rows, cols) to improve readability and maintainability

--- kilo.c
+++ kilo.c
@@ -54,11 +54,10 @@ char editorReadKey() {
 
 int getWindowSize(int *rows, int *cols) {
     struct winsize ws;
-    if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if (1||ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
         if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
             return -1;
-        editorReadKey();
-        return -1;
+        return getCursorPosition(rows, cols);
     } else {
         *cols = ws.ws_col;
         *rows = ws.ws_row;
@@ -66,6 +65,22 @@ int getWindowSize(int *rows, int *cols) {
     }
 }
 
+int getCursorPosition(int *row, int *cols) {
+    if (write(STDIN_FILENO, "\x1b[6n", 4) != 4)
+        return -1;
+    printf("\r\n");
+
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1) {
+        if (iscntrl(c)) {
+            printf("%d\r\n", c);
+        } else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+    }
+    editorReadKey();
+    return -1
+}
 /*** input ***/
 
 void editorProcessKeypress() {
