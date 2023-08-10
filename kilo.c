6b68b98b4ca949b8b9b4a50a3949f71e45adcf39
Priyam Srivastava <ipriyam26@gmail.com>
2023-08-10 23:32:16 +0530

    fix(kilo.c): change enum values for ARROW_LEFT, ARROW_RIGHT, ARROW_UP, and ARROW_DOWN to improve readability and avoid conflicts with ASCII values feat(kilo.c): add support for PAGE_UP and PAGE_DOWN keys to navigate through the editor

--- kilo.c
+++ kilo.c
@@ -13,11 +13,12 @@
 #define CTRL_KEY(k) ((k)&0x1f)
 #define KILO_VERSION "0.0.1"
 enum editorKey {
-    ARROW_UP = 'w',
-    ARROW_DOWN = 's',
-    ARROW_LEFT = 'a',
-    ARROW_RIGHT = 'd',
-
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN,
+    PAGE_UP,
+    PAGE_DOWN
 };
 /*** data ***/
 
@@ -51,7 +52,7 @@ void enableRawMode() {
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
         die("tcsetattr");
 }
-char editorReadKey() {
+int editorReadKey() {
     int nread;
     char c;
     while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
@@ -65,15 +66,28 @@ char editorReadKey() {
         if (read(STDIN_FILENO, &seq[1], 1) != 1)
             return '\x1b';
         if (seq[0] == '[') {
-            switch (seq[1]) {
-            case 'A':
-                return ARROW_UP;
-            case 'B':
-                return ARROW_DOWN;
-            case 'C':
-                return ARROW_RIGHT;
-            case 'D':
-                return ARROW_LEFT;
+            if (seq[1] >= '0' && seq[1] <= '9') {
+                if (read(STDIN_FILENO, &seq[2], 1) != 1)
+                    return '\x1b';
+                if (seq[2] == '~') {
+                    switch (seq[1]) {
+                    case '5':
+                        return PAGE_UP;
+                    case '6':
+                        return PAGE_DOWN;
+                    }
+                }
+            } else {
+                switch (seq[1]) {
+                case 'A':
+                    return ARROW_UP;
+                case 'B':
+                    return ARROW_DOWN;
+                case 'C':
+                    return ARROW_RIGHT;
+                case 'D':
+                    return ARROW_LEFT;
+                }
             }
         }
 
@@ -115,31 +129,46 @@ int getWindowSize(int *rows, int *cols) {
 }
 
 /*** input ***/
-void editorMoveCursor(char key) {
+void editorMoveCursor(int key) {
     switch (key) {
     case ARROW_LEFT:
-        E.cx--;
+
+        if (E.cx != 0) {
+            E.cx--;
+        }
         break;
     case ARROW_RIGHT:
-        E.cx++;
+        if (E.cx != E.screencols - 1) {
+            E.cx++;
+        }
         break;
     case ARROW_UP:
-        E.cy--;
+        if (E.cy != 0) {
+            E.cy--;
+        }
         break;
     case ARROW_DOWN:
-        E.cy++;
+        if (E.cy != E.screenrows - 1) {
+            E.cy++;
+        }
         break;
     }
 }
-
 void editorProcessKeypress() {
-    char c = editorReadKey();
+    int c = editorReadKey();
     switch (c) {
     case CTRL_KEY('q'):
         write(STDOUT_FILENO, "\x1b[2J", 4);
         write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
         break;
+    case PAGE_UP:
+    case PAGE_DOWN: {
+        int times = E.screenrows;
+        while (times--)
+            editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+    } break;
+
     case ARROW_UP:
     case ARROW_DOWN:
     case ARROW_LEFT:
